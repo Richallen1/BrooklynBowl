ddd988b209f2087bcb71eb34fdb9d1cebd2d826d
Richallen1 <rich.allenlx@gmail.com>
2014-03-03 19:12:22 +0000

    Initial Commit

---
+++ BrooklynBowl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BrooklynBowl
+//
+//  Created by Rich Allen on 03/03/2014.
+//  Copyright (c) 2014 Magic Entertainment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BrooklynBowl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BrooklynBowl
+//
+//  Created by Rich Allen on 03/03/2014.
+//  Copyright (c) 2014 Magic Entertainment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
